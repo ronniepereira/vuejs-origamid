b85cf25d9008f1a60749a2eecae272a018a000be
CCM Tecnologia <desenvolvimento@ccmtecnologia.com.br>
2019-09-04 17:38:57 -0300

    Techno em construcao

---
+++ projeto-techno/app.js
@@ -0,0 +1,46 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        produtos: [],
+        produto: false
+    },
+    filters: {
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+        }
+    },
+    methods: {
+        fetchProdutos() {
+            fetch("./api/produtos.json")
+                .then(response => response.json())
+                .then(response => {
+                    this.produtos = response;
+                })
+        },
+
+        fetchProduto(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then(response => response.json())
+                .then(response => {
+                    this.produto = response
+                })
+        },
+
+        abrirModal(id) {
+            this.fetchProduto(id);
+            window.scrollTo({
+                top: 100,
+                behavior: "smooth"
+            });
+        },
+
+        fecharModal({ target, currentTarget }) {
+            if (target === currentTarget) {
+                this.produto = false;
+            }
+        }
+    },
+    created() {
+        this.fetchProdutos();
+    }
+});
